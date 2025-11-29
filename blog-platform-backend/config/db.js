1677cd3e211eb75d2f18e1dc77aa132716f3952c
Chandu Maharana <chandumaharana58537@gmail.com>
2025-11-29 22:03:12 +0530

    Sequelize edited for raailway

--- blog-platform-backend/config/db.js
+++ blog-platform-backend/config/db.js
@@ -1,13 +1,15 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.MYSQLDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
   {
-    host: process.env.DB_HOST,
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
     dialect: "mysql",
-    port: process.env.DB_PORT,
     logging: false
   }
 );
