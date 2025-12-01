45d850217d2e63f53b8a64f3aae8e22afa64acbe
Chandu Maharana <chandumaharana58537@gmail.com>
2025-12-01 19:06:55 +0530

    Sequelize code updated

--- blog-platform-backend/models/post.model.js
+++ blog-platform-backend/models/post.model.js
@@ -1,5 +1,5 @@
 import { DataTypes } from 'sequelize';
-import { sequelize } from '../config/db.js';
+import sequelize from '../config/db.js';
 
 const Post = sequelize.define('Post', {
   title: { type: DataTypes.STRING, allowNull: false },
