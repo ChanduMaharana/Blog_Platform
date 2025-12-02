21145fbb91410e55e534e29befbf1b43f815328c
Chandu Maharana <chandumaharana58537@gmail.com>
2025-12-02 11:34:55 +0530

    backend updated

--- blog-platform-backend/server.js
+++ blog-platform-backend/server.js
@@ -22,7 +22,7 @@ app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 app.use(
   cors({
     origin: [
-      "https://browser-ne7dz238c-220101120198s-projects.vercel.app",
+      "https://blog-platform-xybron-git-master-220101120198s-projects.vercel.app/",
       "http://localhost:4200",
     ],
     credentials: true,
