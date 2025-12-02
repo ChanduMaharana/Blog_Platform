75124296f61cd8732b2c2468b5afffe6b88d95a0
Chandu Maharana <chandumaharana58537@gmail.com>
2025-12-02 12:52:12 +0530

    server.js  updated

--- blog-platform-backend/server.js
+++ blog-platform-backend/server.js
@@ -7,6 +7,7 @@ import postRoutes from "./routes/post.routes.js";
 import commentRoutes from "./routes/comment.routes.js";
 import categoryRoutes from "./routes/category.routes.js";
 import bannerRoutes from "./routes/banner.routes.js";
+import adminRoutes from "./routes/admin.routes.js";
 
 import "./models/admin.model.js";
 import path from "path";
@@ -39,6 +40,7 @@ process.on("unhandledRejection", (err) => {
   console.error("PROMISE ERROR ->", err);
 });
 
+app.use("/api/admin", adminRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
 app.use("/api/categories", categoryRoutes);
