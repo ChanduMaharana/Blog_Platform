4bad1ec3e1516a871c2821b3d04b2354d5443c91
Chandu Maharana <chandumaharana58537@gmail.com>
2025-12-03 23:27:01 +0530

    updated functionality for metric cards in dahboard

--- frontend/src/app/core/admin/dashboard-cards/dashboard-cards.ts
+++ frontend/src/app/core/admin/dashboard-cards/dashboard-cards.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PostService } from '../../../services/post-service';
+import { CategoryService } from '../../../services/category-service';
+import { BannerService } from '../../../services/banner-service';
 
 @Component({
   selector: 'app-dashboard-cards',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './dashboard-cards.html',
 })
-export class DashboardCards {
+export class DashboardCards implements OnInit {
 
+  postCount: number = 0;
+  categoryCount: number = 0;
+  bannerCount: number = 0;
+
+  constructor(
+    private postService: PostService,
+    private categoryService: CategoryService,
+    private bannerService: BannerService
+  ) {}
+
+  ngOnInit(): void {
+    this.loadCounts();
+  }
+
+  loadCounts() {
+    this.postService.list().subscribe((posts) => {
+      this.postCount = posts.length;
+    });
+
+    this.categoryService.getAll().subscribe((cats) => {
+      this.categoryCount = cats.length;
+    });
+
+    this.bannerService.getAll().subscribe((banners) => {
+      this.bannerCount = banners.length;
+    });
+  }
 }
