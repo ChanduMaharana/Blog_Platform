c3f37118a944e6de58d983be107afdee5d5ec0e2
Chandu Maharana <chandumaharana58537@gmail.com>
2025-12-05 23:06:45 +0530

    Post image issue resolved

--- blog-platform-backend/routes/post.routes.js
+++ blog-platform-backend/routes/post.routes.js
@@ -5,9 +5,9 @@ import { upload } from "../middleware/uploads.js";
 import {
   createPost,
   getPosts,
-  // getPostById,
-  // updatePost,
-  // deletePost,
+  getPostById,
+  updatePost,
+  deletePost,
   getPaginatedPosts
 } from "../controllers/post.controller.js";
 import { getComments, addComment } from "../controllers/comment.controller.js";
@@ -16,9 +16,9 @@ const router = express.Router();
 
 
 router.get("/", getPosts);
-// router.get("/:id", getPostById);
-// router.put("/:id", updatePost);
-// router.delete("/:id", deletePost);
+router.get("/:id", getPostById);
+router.put("/:id", updatePost);
+router.delete("/:id", deletePost);
 router.get("/paginated/list", getPaginatedPosts);
 router.post("/", upload.single("image"), createPost);
 
