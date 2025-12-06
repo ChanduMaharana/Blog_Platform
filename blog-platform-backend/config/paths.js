44cd742015508e0068afccf5bc3f9345dcb575e8
Chandu Maharana <chandumaharana58537@gmail.com>
2025-12-06 16:06:43 +0530

    The backend served fine

--- blog-platform-backend/config/paths.js
+++ blog-platform-backend/config/paths.js
@@ -7,4 +7,3 @@ export const __dirname = path.dirname(__filename);
 export const ROOT_DIR = path.join(__dirname, "..");
 export const UPLOADS_PATH = path.join(ROOT_DIR, "uploads");
 export const BANNERS_PATH = path.join(ROOT_DIR, "uploads/banners");
-a
