2ad46f1d8a57c4918f870ff4b41c626802e8fa2c
Chandu Maharana <chandumaharana58537@gmail.com>
2025-12-11 18:24:23 +0530

    Responsive design were added

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -9,12 +9,16 @@ import { AuthGuard } from './services/auth/auth.guard';
 export const routes: Routes = [
   
   { path: '', redirectTo: 'home', pathMatch: 'full' },
+
   { path: 'home', component: Home },
+
   { path: 'posts', component: Postlist },
   { path: 'posts/:id', component: Postdetails },
-  { path: 'login', component: Login },
-  { path: 'admin/dashboard', component: AdminDashboard, canActivate: [AuthGuard] },
-  { path: '', component: Postlist, runGuardsAndResolvers: 'always' },
-  { path: '**', redirectTo: 'home' }
 
+  { path: 'login', component: Login },
+
+  { path: 'admin/dashboard', component: AdminDashboard, canActivate: [AuthGuard] },
+
+  { path: '**', redirectTo: 'home' }
 ];
+
