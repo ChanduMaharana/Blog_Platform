b64d56ee70f2eb8a5d6a405176c6ef89d45a8c65
Chandu Maharana <chandumaharana58537@gmail.com>
2025-12-13 18:49:20 +0530

    SEO setting issue resolving

--- frontend/src/main.server.ts
+++ frontend/src/main.server.ts
@@ -1,18 +1,5 @@
-import { BootstrapContext, bootstrapApplication } from '@angular/platform-browser';
-import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
-
+import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { config } from './app/app.config.server';
 
-const bootstrap = (context: BootstrapContext) =>
-  bootstrapApplication(App, {
-    ...config,
-    providers: [
-      ...(config.providers ?? []),
-      provideClientHydration(),
-      provideHttpClient(withFetch())   
-    ]
-  }, context);
-
-export default bootstrap;
+export default () => bootstrapApplication(App, config);
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,26 +1,6 @@
-import 'zone.js';
-import { bootstrapApplication, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { appConfig } from './app/app.config';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
-import { LucideAngularModule, Facebook, Twitter, Linkedin, MessageCircle, Link } from 'lucide-angular';
-import { importProvidersFrom } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 
-bootstrapApplication(App, {
-  ...appConfig,
-  providers: [
-    provideRouter(routes),
-    importProvidersFrom(
-      HttpClientModule,
-      LucideAngularModule.pick({
-        Facebook,
-        Twitter,
-        Linkedin,
-        MessageCircle,
-        Link,
-      })
-    ), provideClientHydration(withEventReplay()),
-  ],
-});
+bootstrapApplication(App, appConfig)
+  .catch(err => console.error(err));
