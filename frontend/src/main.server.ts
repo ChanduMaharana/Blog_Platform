cdaf9e808befd59cabeb7e313dce254ff31b729d
Chandu Maharana <chandumaharana58537@gmail.com>
2025-12-17 21:54:27 +0530

    Cleared vercel deployment issue

--- frontend/src/main.server.ts
+++ frontend/src/main.server.ts
@@ -1,16 +1,22 @@
+import 'zone.js';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { provideServerRendering } from '@angular/platform-server';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+
 import { App } from './app/app';
 import { routes } from './app/app.routes';
-import { bootstrapApplication } from '@angular/platform-browser';
 
-export default function bootstrap() {
-  return bootstrapApplication(App, {
-    providers: [
-      provideServerRendering(),   
-      provideRouter(routes),
-      provideHttpClient()
-    ]
-  });
+export default function bootstrap(context: any) {
+  return bootstrapApplication(
+    App,
+    {
+      providers: [
+        provideServerRendering(),
+        provideRouter(routes),
+        provideHttpClient()
+      ]
+    },
+    context 
+  );
 }
