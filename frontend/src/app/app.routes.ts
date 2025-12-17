4033a78dae87578842c961ad8861c51770f31b86
Chandu Maharana <chandumaharana58537@gmail.com>
2025-12-17 20:05:40 +0530

    Config file changed

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,28 +4,19 @@ import { Postlist } from './core/postlist/postlist';
 import { Postdetails } from './core/postdetails/postdetails';
 import { AdminDashboard } from './core/admin/admin-dashboard/admin-dashboard';
 import { Login } from './core/login/login';
-import { AuthGuard } from './services/auth/auth.guard';
-import { PostResolver } from './shared/post.resolver';
 
 export const routes: Routes = [
-  
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 
   { path: 'home', component: Home },
 
   { path: 'posts', component: Postlist },
-  // { path: 'posts/:id', component: Postdetails },
-   {
-  path: 'posts/:id',
-  component: Postdetails,
-  resolve: { post: PostResolver }
-},
 
+  { path: 'posts/:id', component: Postdetails },
 
   { path: 'login', component: Login },
 
-  { path: 'admin/dashboard', component: AdminDashboard, canActivate: [AuthGuard] },
+  { path: 'admin/dashboard', component: AdminDashboard },
 
   { path: '**', component: Home }
 ];
-
