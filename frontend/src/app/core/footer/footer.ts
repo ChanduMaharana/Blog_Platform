1f0b6e215e73d41da1316c81baa7a6b1c1711c10
Chandu Maharana <chandumaharana58537@gmail.com>
2025-12-30 11:04:12 +0530

    The category issue is fixed

--- frontend/src/app/core/footer/footer.ts
+++ frontend/src/app/core/footer/footer.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -10,4 +11,13 @@ import { FormsModule } from '@angular/forms';
 })
 export class Footer {
 
+  categories = ['News', 'Business', 'Politics','Sports', 'Entertainment', 'Tech'];
+
+  constructor(private router: Router) {}
+
+ filterByCategory(category: string) {
+    this.router.navigate(['/home'], {
+      queryParams: { category }
+    });
+  }
 }
