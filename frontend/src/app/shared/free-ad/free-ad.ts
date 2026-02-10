89b7b3261c609cc42c0b135e766d2990608bd018
Chandu Maharana <chandumaharana58537@gmail.com>
2026-02-10 11:07:19 +0530

    Fixed Banners issue

--- frontend/src/app/shared/free-ad/free-ad.ts
+++ frontend/src/app/shared/free-ad/free-ad.ts
@@ -1,22 +1,27 @@
-import { Component ,Input, AfterViewInit} from '@angular/core';
+import { Component, Input, AfterViewInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-free-ad',
-  imports: [],
-  templateUrl: './free-ad.html',
+  standalone: true,
+  template: `
+    <div class="ad-container" #adHost></div>
+  `,
 })
 export class FreeAd implements AfterViewInit {
-  @Input() zoneId!: string;   
-  @Input() width = 300;
-  @Input() height = 250;
+  @Input() zoneId!: string;
+
+  constructor(private el: ElementRef) {}
 
   ngAfterViewInit() {
+    const container = this.el.nativeElement.querySelector('.ad-container');
+
+    if (!container || container.childNodes.length) return;
+
     const script = document.createElement('script');
     script.type = 'text/javascript';
     script.async = true;
     script.src = `https://www.topcreativeformat.com/${this.zoneId}/invoke.js`;
-    document.body.appendChild(script);
+
+    container.appendChild(script);
   }
 }
-
-
