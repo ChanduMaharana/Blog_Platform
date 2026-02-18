e4d3e59e40fc19e8f0b7e6058f139df34d4ef44c
Chandu Maharana <chandumaharana58537@gmail.com>
2026-02-18 19:50:12 +0530

    Fixed ads blocks

--- frontend/src/app/shared/free-ad/free-ad.ts
+++ frontend/src/app/shared/free-ad/free-ad.ts
@@ -3,24 +3,21 @@ import { Component, Input, AfterViewInit, ElementRef } from '@angular/core';
 @Component({
   selector: 'app-free-ad',
   standalone: true,
-  template: `
-    <div class="ad-container" #adHost></div>
-  `,
+  template: `<div class="ad-container"></div>`
 })
 export class FreeAd implements AfterViewInit {
-  @Input() zoneId!: string;
+
+  @Input() scriptUrl!: string;
 
   constructor(private el: ElementRef) {}
 
-  ngAfterViewInit() {
+  ngAfterViewInit(): void {
     const container = this.el.nativeElement.querySelector('.ad-container');
-
-    if (!container || container.childNodes.length) return;
 
     const script = document.createElement('script');
     script.type = 'text/javascript';
+    script.src = this.scriptUrl;
     script.async = true;
-    script.src = `https://www.topcreativeformat.com/${this.zoneId}/invoke.js`;
 
     container.appendChild(script);
   }
