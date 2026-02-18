65ac7af0639842ada7733327a5bf069c05be86a0
Chandu Maharana <chandumaharana58537@gmail.com>
2026-02-18 20:41:28 +0530

    Fixed ads blocks

--- frontend/src/app/shared/free-ad/free-ad.ts
+++ frontend/src/app/shared/free-ad/free-ad.ts
@@ -15,22 +15,25 @@ export class FreeAd implements AfterViewInit {
   ngAfterViewInit() {
   setTimeout(() => {
     const script1 = document.createElement('script');
+    script1.type = 'text/javascript';
     script1.innerHTML = `
       var atOptions = {
-        'key' : '${this.adKey}',
+        'key' : 'a88dfd6feba69be6dc45f290686fbebe',
         'format' : 'iframe',
-        'height' : ${this.height},
-        'width' : ${this.width},
+        'height' : 90,
+        'width' : 728,
         'params' : {}
       };
     `;
 
     const script2 = document.createElement('script');
-    script2.src = `https://www.highperformanceformat.com/${this.adKey}/invoke.js`;
+    script2.type = 'text/javascript';
+    script2.src = 'https://www.highperformanceformat.com/a88dfd6feba69be6dc45f290686fbebe/invoke.js';
     script2.async = true;
 
     this.el.nativeElement.appendChild(script1);
     this.el.nativeElement.appendChild(script2);
   }, 500);
 }
+
 }
