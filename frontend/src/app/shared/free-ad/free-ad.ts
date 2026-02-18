b35c17e63e546e73fc05087079dd516f9a847006
Chandu Maharana <chandumaharana58537@gmail.com>
2026-02-18 20:07:16 +0530

    Fixed ads blocks

--- frontend/src/app/shared/free-ad/free-ad.ts
+++ frontend/src/app/shared/free-ad/free-ad.ts
@@ -7,18 +7,36 @@ import { Component, Input, AfterViewInit, ElementRef } from '@angular/core';
 })
 export class FreeAd implements AfterViewInit {
 
-  @Input() scriptUrl!: string;
+  @Input() adKey!: string;
+  @Input() width = 728;
+  @Input() height = 90;
 
   constructor(private el: ElementRef) {}
 
   ngAfterViewInit(): void {
     const container = this.el.nativeElement.querySelector('.ad-container');
 
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = this.scriptUrl;
-    script.async = true;
+    // Create atOptions script
+    const optionsScript = document.createElement('script');
+    optionsScript.type = 'text/javascript';
+    optionsScript.innerHTML = `
+      var atOptions = {
+        'key' : '${this.adKey}',
+        'format' : 'iframe',
+        'height' : ${this.height},
+        'width' : ${this.width},
+        'params' : {}
+      };
+    `;
 
-    container.appendChild(script);
+    container.appendChild(optionsScript);
+
+    // Create invoke script
+    const invokeScript = document.createElement('script');
+    invokeScript.type = 'text/javascript';
+    invokeScript.src = `https://www.highperformanceformat.com/${this.adKey}/invoke.js`;
+    invokeScript.async = true;
+
+    container.appendChild(invokeScript);
   }
 }
